1eb057d5138a2bd5f5a7e951cd36914575e4107c
mamunr412 <mamunr412@gmail.com>
2021-09-26 22:46:18 +0600

    budget add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="header-conatiner">
             <h1>Meeting With Astronuat</h1>
             <h4>Will 2021 Be the Year We Find Intelligent Alien Life?</h4>
-            <h2>Misson Budget: 500 Million </h2>
+            <h2>Misson Budget: 900 Million </h2>
         </div>
     );
 };
